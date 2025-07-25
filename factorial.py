9613d9420433dba0d0575ec6cc11afc75f1daa8c
Madhure-xcoder <madhumaithili647@gmail.com>
2025-07-25 11:02:03 +0530

    pattern print

--- factorial.py
+++ factorial.py
@@ -1,6 +1,16 @@
-n = int(input("Enter val: "))
+# factorial using for loop
+n=int(input("Enter n: "))
 fact=1
-for i in range(1, n+1):
-     fact*=i
-     
-print("factorial is: ",fact)
+for i in range(1,n+1):
+    fact*=i
+print(fact)
+
+
+# using while loop
+n=int(input("Enter n: "))
+fact=1
+i=1
+while i<=n:
+    fact*=i
+    i+=1
+print(fact)
